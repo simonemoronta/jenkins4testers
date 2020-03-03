22efd8763009db91c26ce2dbe395a827342cc40a
t0210smn <t0210smn@prestadorcbmp.cm.br>
2020-03-03 14:27:23 -0300

    Atualização do env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,7 +16,17 @@ when "firefox"
 when "chrome"
   @driver = :selenium_chrome
 when "headless"
-  @driver = :selenium_chrome_headless
+  Capybara.register_driver :selenium_chrome_headless do |app|
+  # Capybara::Selenium::Driver.load_selenium
+  chrome_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.args << '--headless'
+    opts.args << '--disable-gpu' if Gem.win_platform?
+    opts.args << '--no-sandbox'
+    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
+    opts.args << '--disable-site-isolation-trials'
+  end
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
 else
   puts "Invalid Browser"
 end
